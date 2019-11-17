927619ec78f9a9d6f5d6f108baa0dd64894a197c
root <root@ip-172-31-32-100.us-east-2.compute.internal>
2019-11-17 10:33:33 +0000

    (*(&

---
+++ cft-cli.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name myteststack --template-body file://vpc.txt --region us-east-2
--- cli.sh
+++
@@ -1,1 +0,0 @@
- aws ec2 describe-volumes --query "Volumes[*].{ID:VolumeId,Tag:Tags}" --region us-east-2 --output text
